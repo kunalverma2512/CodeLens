f17ea42352de1007485bd0288c83ceead798cc4a
Harkirat <sainiharkirat38@gmail.com>
2026-05-15 14:07:49 +0530

    fix : remove footer strip comment

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -296,7 +296,6 @@ export default function Navbar() {
                   })}
                 </div>
 
-                {/* Footer strip */}
                 
               </div>
             </div>
