2d74b23133f280ae82b54346e4d07bfcd440bb3d
anushreekhatri028-coder <anushreekhatri028@gmail.com>
2026-06-16 13:33:19 +0530

    new-logo

--- frontend/src/components/shared/loaders/LoaderAlt.jsx
+++ frontend/src/components/shared/loaders/LoaderAlt.jsx
@@ -3,7 +3,7 @@ const Spinner = () => {
     <div
       role="status"
       aria-live="polite"
-      className="min-h-screen flex items-center justify-center bg-white"
+      className="min-h-screen flex flex-col items-center justify-center bg-white"
     >
       <span className="sr-only">Loading, please wait...</span>
 
